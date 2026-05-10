37394aeb67f412f3a0c59d24eeead0c4f4818872
HaoranTech <cao_haoran@icloud.com>
2026-05-10 09:24:14 +0800

    Initial Commit

---
+++ Swift Zip Manager For iOS/Swift_Zip_Manager_For_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Zip_Manager_For_iOSApp.swift
+//  Swift Zip Manager For iOS
+//
+//  Created by Haoran on 2026/5/10.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Zip_Manager_For_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
